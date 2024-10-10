55217510a55518e6f2038f78b6bee2a14802f2ee
Maxim <122220205+Hhtitg1234@users.noreply.github.com>
2024-10-11 00:27:05 +0300

    NEW RELEASE!!! 2 LANGUAGES (ENG, RUS)
    To start playing,
    1) download all files.
    2) create a folder with any name.
    3) put the files in it.
    4) log into PyCharm or another program.
    5) open the file folder in the program.
    6) create a virtual environment if it is not worth it.
    7) download the time, keyboard libraries.
    8) run the file (main_2_0.py).

---
+++ functions_eng.py
@@ -0,0 +1,30 @@
+# Для магаза энергии
+def shop_list_energy():
+    print("Welcome to shop! Do U want to buy smth?\n")
+    print("To buy press NUMBER\n")
+    print("1) store of electricity: 1000 kW. Cost: 250\n"
+          "C-close раздел")
+
+
+# Для магаза майнеров
+def shop_list_1():
+    print("Welcome to shop! Do U want to buy smth?\n")
+    print("To buy press NUMBER\n")
+    print("1) 1 lvl miner. Coins per second: 2. Energy consumption: 3. Cost: 20\n"
+          "2) 2 lvl miner. Coins per second: 5. Energy consumption: 6. Cost: 120\n"
+          "C-exit to the section menu\n")
+
+
+def shop_list_2():
+    print("Welcome to shop! Do U want to buy smth?\n")
+    print("To buy press NUMBER\n")
+    print("1) 3 lvl miner. Coins per second: 10. Energy consumption: 15. Cost: 350\n"
+          "2) 4 lvl miner. Coins per second: 13. Energy consumption: 14. Cost: 550\n"
+          "C-exit to the section menu\n")
+
+
+def shop_list_3():
+    print("Welcome to shop! Do U want to buy smth?\n")
+    print("To buy press NUMBER\n")
+    print("1) 5 lvl miner. Coins per second: 15. Energy consumption: 14. Cost: 700\n"
+          "C-exit to the section menu\n")
